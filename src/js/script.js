7f2a9b8cb0db6f204224dfa420b476e7e893d768
wongsrila <jiraphong.wongsrila@gmail.com>
2023-02-13 23:34:38 +0100

    het werkt eindelijk ?

--- src/js/script.js
+++ src/js/script.js
@@ -1,18 +1,34 @@
-function onScanSuccess(qrCodeMessage) {
-  // handle the scanned code as you like, for example:
-  document.getElementById('result').innerHTML =
-    '<span class="result">' + qrCodeMessage + '</span>';
+// function onScanSuccess(qrCodeMessage) {
+//   document.getElementById('result').innerHTML =
+//     '<span class="result">' + qrCodeMessage + '</span>';
+// }
+
+// function onScanFailure(error) {}
+
+// let html5QrcodeScanner = new Html5QrcodeScanner(
+//   'reader',
+//   { fps: 10, qrbox: { width: 250, height: 250 } },
+//   false,
+// );
+// html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+
+// BARCODE API
+function onScanSuccess(decodedText, decodedResult) {
+  // console.log(`Code matched = ${decodedText}`, decodedResult);
+  fetch(`https://world.openfoodfacts.org/api/v0/product/${decodedText}.json`)
+    .then((response) => response.json())
+    .then((data) => console.log(data));
 }
 
 function onScanFailure(error) {
-  // handle scan failure, usually better to ignore and keep scanning.
-  // for example:
   // console.warn(`Code scan error = ${error}`);
 }
 
 let html5QrcodeScanner = new Html5QrcodeScanner(
   'reader',
-  { fps: 10, qrbox: { width: 250, height: 250 } },
-  /* verbose= */ false,
+  { fps: 20, qrbox: { width: 250, height: 250 } },
+  false,
 );
 html5QrcodeScanner.render(onScanSuccess, onScanFailure);
+
+// FOOD API
