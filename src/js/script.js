68677c99221e8e0457854afc01820faebc57ab50
wongsrila <jiraphong.wongsrila@gmail.com>
2023-02-13 19:43:41 +0100

    13feb 19:43

--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,9 @@
+const pTag = document.querySelector('p');
+
 function onScanSuccess(decodedText, decodedResult) {
   // handle the scanned code as you like, for example:
-  console.log(`Code matched = ${decodedText}`, decodedResult);
+  // console.log(`Code matched = ${decodedText}`, decodedResult);
+  pTag.innerHTML = `Code matched = ${decodedText}`;
 }
 
 function onScanFailure(error) {
