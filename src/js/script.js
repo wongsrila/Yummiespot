dbe62abc554c6ee5e03543cbd40a322d69075cdd
wongsrila <jiraphong.wongsrila@gmail.com>
2023-02-14 11:42:03 +0100

    14 feb 11:42

--- src/js/script.js
+++ src/js/script.js
@@ -1,34 +1,63 @@
-// function onScanSuccess(qrCodeMessage) {
-//   document.getElementById('result').innerHTML =
-//     '<span class="result">' + qrCodeMessage + '</span>';
+const dataResult = document.querySelector('#result');
+const stopScanning = document.querySelector('#stop-scanning');
+const startScanning = document.querySelector('#start-scanning');
+
+// function onScanSuccess(decodedText) {
+//   fetch(`https://world.openfoodfacts.org/api/v0/product/${decodedText}.json`)
+//     .then((response) => response.json())
+//     .then((data) => dataInfo(data));
 // }
 
 // function onScanFailure(error) {}
 
 // let html5QrcodeScanner = new Html5QrcodeScanner(
 //   'reader',
-//   { fps: 10, qrbox: { width: 250, height: 250 } },
+//   { fps: 20, qrbox: { width: 250, height: 250 } },
 //   false,
 // );
 // html5QrcodeScanner.render(onScanSuccess, onScanFailure);
 
-// BARCODE API
-function onScanSuccess(decodedText, decodedResult) {
-  // console.log(`Code matched = ${decodedText}`, decodedResult);
-  fetch(`https://world.openfoodfacts.org/api/v0/product/${decodedText}.json`)
-    .then((response) => response.json())
-    .then((data) => console.log(data));
-}
+// function dataInfo(data) {
+//   console.log(data);
+//   dataResult.innerHTML = data.product.brands;
+// }
 
-function onScanFailure(error) {
-  // console.warn(`Code scan error = ${error}`);
-}
+startScanning.addEventListener('click', () => {
+  Html5Qrcode.getCameras()
+    .then((devices) => {
+      if (devices && devices.length) {
+        var cameraId = devices[0].id;
+        const html5QrCode = new Html5Qrcode('reader');
+        html5QrCode
+          .start(
+            cameraId,
+            {
+              fps: 10,
+              qrbox: { width: 250, height: 250 },
+            },
+            (decodedText) => {
+              console.log(decodedText);
+              dataResult.innerHTML = decodedText;
+            },
+            (errorMessage) => {},
+          )
+          .catch((err) => {
+            console.log(err);
+          });
 
-let html5QrcodeScanner = new Html5QrcodeScanner(
-  'reader',
-  { fps: 20, qrbox: { width: 250, height: 250 } },
-  false,
-);
-html5QrcodeScanner.render(onScanSuccess, onScanFailure);
-
-// FOOD API
+        stopScanning.addEventListener('click', () => {
+          html5QrCode
+            .stop()
+            .then((ignore) => {
+              // QR Code scanning is stopped.
+            })
+            .catch((err) => {
+              // Stop failed, handle it.
+            });
+        });
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+});
